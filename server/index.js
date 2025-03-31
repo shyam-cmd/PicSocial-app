221f55257aa90be2c565a0aa5694aa4f548751a0
shyam pathade <shyam.pathade55@gmail.com>
2025-03-31 14:37:29 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -13,12 +13,16 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = 'mongodb+srv://mejorp:shyampass@cluster0.9tnkvf3.mongodb.net/memories?retryWrites=true&w=majority&appName=Cluster0'
+// const CONNECTION_URL = 'mongodb+srv://mejorp:@cluster0.mongodb.net/memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+//   .catch((error) => console.log(`${error} did not connect`));
+
+  mongoose.connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('findOneAndUpdate', false);
